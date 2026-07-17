388508ea556efe3adb3ff8a0ca0b89791ac6565e
MohanSusheet <susheetm@gmail.com>
2026-07-18 00:34:15 +0530

    Binary Tree Top View

---
+++ BinaryTree/TopViewOfBinaryTree.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<set>
+#include<map>
+
+using namespace std;
+
+class TreeNode
+{
+public: 
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(){} //Empty Constructor
+    TreeNode(int val)
+    {
+        this->val = val;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+    TreeNode(int val, TreeNode* left, TreeNode* right)
+    {
+        this->val = val;
+        this->left = left;
+        this->right = right;
+    }
+};
+
+vector<int> topViewOfBinaryTree(TreeNode* root)
+{
+    //T.C --> O(N)*log N [multiset], S.C -->O(N)[map] + O(N)[queue] ~~ O(N) 
+    vector<int> ans;
+    if(root == NULL)return ans;
+    
+    map<int,int> m;
+    queue<pair<int,TreeNode*>>q;
+    
+    q.push({0, root});
+    
+    while(!q.empty())
+    {
+        int size = q.size();
+        
+        for(int i=0; i < size; i++)
+        {
+            pair<int, TreeNode*> node = q.front();
+            q.pop();
+        
+            int v = node.first;
+            if(m.find(v) == m.end())m[v] = node.second->val;
+            if(node.second->left)q.push({v-1, node.second->left});
+            if(node.second->right)q.push({v+1, node.second->right});
+        }
+    }
+    
+    for(auto it: m)
+    {
+        ans.push_back(it.second);
+    }
+    
+    return ans;
+}
+
+int main()
+{
+    TreeNode* root = new TreeNode(8);
+    root->left = new TreeNode(3);
+    root->left->left = new TreeNode(1);
+    root->left->right = new TreeNode(6);
+    root->left->right->left = new TreeNode(4);
+    root->left->right->right = new TreeNode(7);
+    root->right = new TreeNode(10);
+    root->right->left = new TreeNode(11);
+    root->right->right = new TreeNode(14);
+    root->right->right->left = new TreeNode(13);
+    root->right->right->left->right = new TreeNode(19);
+
+    vector<int> result = topViewOfBinaryTree(root);
+
+    cout<<"Top view of Binary Tree: ";
+    for(auto it: result)
+    {
+        cout<<it<<" ";
+    }
+    
+    return 0;
+}
